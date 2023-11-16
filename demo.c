3aafe3843ca1dcce8046c75cc62b512a6dbb5291
yangjiule <yang_jiule@163.com>
2023-11-16 20:34:59 +0800

    first

---
+++ demo.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("这是一个测试demo");
+    return 0;
+}
---
+++ demo.java
@@ -0,0 +1,6 @@
+public class demo {
+    public static void main(String[]args)
+    {
+        System.out.println("这是一个测试demo");
+    }
+}
